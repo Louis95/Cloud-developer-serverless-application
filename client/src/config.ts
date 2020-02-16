cb07eff56f45b9bfb72877435e01f8491e54ec94
Pila louis <plouisasobo@gmail.com>
2020-02-16 19:32:21 +0100

    complete implementation

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,10 +1,10 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '...'
+const apiId = 'w5tormob87'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'serverless-todo-app.auth0.com',            // Auth0 domain
+  clientId: 'uI3y1v25i35yGx9IEJG1X7UirHWoEzUK',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
